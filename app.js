2c5c11091ad0fee42f772f7f7f778ee6b2582ed7
sungeun <sachoi1201@naver.com>
2022-05-24 02:56:01 +0900

    5/24

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ function filter(content) {
     /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
   const specialRegex = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
 
-  content.replace(emoticonRegex, '');
-  content.replace(specialRegex, '');
+  // content.replace(emoticonRegex, '');
+  // content.replace(specialRegex, '');
   if (content.length < 2000 && content.length > 200) {
     return 1;
   }
@@ -51,7 +51,7 @@ app.post('/api/summarize', async (req, res) => {
   if (filter(content)) {
     const postData = {
       document: {
-        title,
+        title: 'title',
         content,
       },
       option: {
@@ -81,11 +81,15 @@ app.post('/api/summarize', async (req, res) => {
 });
 
 app.post('/api/translate', async (req, res) => {
-  const { text } = req.body;
+  let { text } = req.body;
+  const englishFilter = /[^a-zA-Z,. ]/g;
+
+  let text1 = text.replace(englishFilter, '');
+  console.log(text1);
   const postData = {
     source: 'en',
     target: 'ko',
-    text,
+    text1,
   };
   try {
     const { data } = await axios.post(Papago_baseUrl, postData, {
@@ -95,6 +99,7 @@ app.post('/api/translate', async (req, res) => {
         'Content-Type': 'application/json',
       },
     });
+    console.log(data.message.result);
     res.send(data.message.result);
   } catch (error) {
     throw new Error(error);
