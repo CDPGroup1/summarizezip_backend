1686c22487ff28ffda228c03f7a710ff73e73c2f
sungeun <sachoi1201@naver.com>
2022-05-24 13:15:29 +0900

    qwe

--- app.js
+++ app.js
@@ -18,13 +18,9 @@ const Clova_baseUrl = 'https://naveropenapi.apigw.ntruss.com/text-summary/v1/sum
 const Papago_baseUrl = 'https://openapi.naver.com/v1/papago/n2mt';
 
 function filter(content) {
-  const emoticonRegex =
-    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-  const specialRegex = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
-
-  // content.replace(emoticonRegex, '');
-  // content.replace(specialRegex, '');
-  if (content.length < 2000 && content.length > 200) {
+  const reg = /[^a-zA-Z,. ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
+  const filterContent = content.replace(reg, '');
+  if (filterContent.length < 2000 && filterContent.length > 200) {
     return 1;
   }
   return 0;
@@ -81,15 +77,14 @@ app.post('/api/summarize', async (req, res) => {
 });
 
 app.post('/api/translate', async (req, res) => {
-  let { text } = req.body;
+  const { text } = req.body;
   const englishFilter = /[^a-zA-Z,. ]/g;
 
-  let text1 = text.replace(englishFilter, '');
-  console.log(text1);
+  const filterText = text.replace(englishFilter, '');
   const postData = {
     source: 'en',
     target: 'ko',
-    text1,
+    text: filterText,
   };
   try {
     const { data } = await axios.post(Papago_baseUrl, postData, {
